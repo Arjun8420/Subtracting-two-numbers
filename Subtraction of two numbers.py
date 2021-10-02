45bf34a11403c3e1793bcf3398bb2adcc8fc3b0e
Arjun M <90825849+Arjun8420@users.noreply.github.com>
2021-10-02 15:32:23 +0530

    Add files via upload

---
+++ Subtraction of two numbers.py
@@ -0,0 +1,5 @@
+#Subtraction  of two numbers
+a = float(input("Enter first number for subtraction = "))
+b = float(input("Enter second number for subraction = "))
+c = a - b
+print("Answer is = ",c)
